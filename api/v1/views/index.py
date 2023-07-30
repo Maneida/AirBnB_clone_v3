c69da33979c5383c2b4a4ba02ce452e87d221ba7
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-30 23:15:44 +0100

    Updated Import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonifyi
+from models import storage
 
 @app_views.route('/', defaults={'page': 'index'})
 @app_views.route('/<page>')
