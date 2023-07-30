3942a491629af41ce4e3b26364c367efbfe153a5
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-30 23:08:21 +0100

    Task 4: stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,21 @@ def api_status(page):
         JSON object: {"status": "OK"}
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """Gets stat of all class data"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    all_cls = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+    stats_dict = {}
+
+    for i in range(len(all_cls)):
+        stats_dict[names[i]] = storage.count(all_cls[i])
+    return jsonify(stats_dict)
