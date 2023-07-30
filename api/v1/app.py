5b116ebb30bed432115e35f50a0eab1048f508fd
Michael Amuah <mkdamuah@hotmail.com>
2023-07-30 21:02:44 +0000

    task 3: Status of API

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception=None):
+    """Close the database storage connection after each request.
+
+    Args:
+        exception (Exception): The exception that occurred, if any.
+    """
+    storage.close
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/', defaults={'page': 'index'})
+@app_views.route('/<page>')
+def api_status(page):
+    """Get the status of the API.
+
+    Returns:
+        JSON object: {"status": "OK"}
+    """
+    return jsonify({"status": "OK"})
