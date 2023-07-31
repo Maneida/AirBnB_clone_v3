6aa1821eb939b2ea0662b1c6d433970bdb8c5ca7
Michael Amuah <mkdamuah@hotmail.com>
2023-07-31 00:05:18 +0000

    task 5: Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 from flask import Flask, Blueprint
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 import os
 
 app = Flask(__name__)
@@ -17,8 +18,12 @@ def close_storage(exception=None):
     """
     storage.close
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
